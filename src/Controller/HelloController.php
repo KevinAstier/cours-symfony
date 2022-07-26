2c4b753346a9b79f2989f67ded44d2e692179a1e
KevinAstier <kevin.astier@humansconnexion.com>
2022-07-26 18:02:16 +0200

    container de Services - à l'oeuvre

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,19 +2,25 @@
 
 namespace App\Controller;
 
+use App\Taxes\Calculator;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController {
 
+    protected $calculator;
+
+    public function __construct(Calculator $calculator) {
+        $this->calculator = $calculator;
+    }
     /**
      * @Route("/hello/{name<[A-z]+>?World}",
      *     name="hello",
      *     methods={"GET", "POST"},
      *     host="127.0.0.1",
      *     schemes={"http", "https"},
-     *
      * )
      * @param Request $request
      * @param $name
@@ -22,6 +28,8 @@ class HelloController {
      */
     public function hello(Request $request, $name) {
 
+        $tva = $this->calculator->calcul(100);
+        dump($tva);
         return new Response("Hello $name");
     }
 }
