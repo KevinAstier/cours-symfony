f3bcb052cfbbb4bc0e94c7a09efdf16fb032f96f
KevinAstier <kevin.astier@humansconnexion.com>
2022-07-27 12:32:28 +0200

    Découverte de la notion de container

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -3,18 +3,15 @@
 namespace App\Controller;
 
 use App\Taxes\Calculator;
+use Cocur\Slugify\Slugify;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class HelloController {
 
-    protected $calculator;
-
-    public function __construct(Calculator $calculator) {
-        $this->calculator = $calculator;
-    }
     /**
      * @Route("/hello/{name<[A-z]+>?World}",
      *     name="hello",
@@ -22,13 +19,13 @@ class HelloController {
      *     host="127.0.0.1",
      *     schemes={"http", "https"},
      * )
-     * @param Request $request
-     * @param $name
-     * @return Response
      */
-    public function hello(Request $request, $name) {
+    public function hello($name = "World", LoggerInterface $logger, Calculator $calculator, Slugify $slugify, Environment $twig) {
 
-        $tva = $this->calculator->calcul(100);
+        dump(($twig));
+        dump($slugify->slugify("Hello World"));
+        $logger->error("Mon message de log !");
+        $tva = $calculator->calcul(100);
         dump($tva);
         return new Response("Hello $name");
     }
--- src/Taxes/Calculator.php
+++ src/Taxes/Calculator.php
@@ -6,9 +6,11 @@ use Psr\Log\LoggerInterface;
 
 class Calculator {
     protected $logger;
+    protected $tva;
 
-    public function __construct(LoggerInterface $logger) {
+    public function __construct(LoggerInterface $logger, float $tva) {
         $this->logger = $logger;
+        $this->tva = $tva;
     }
     public function calcul(float $prix) : float {
         $this->logger->info("Un calcul a lieu : $prix");
