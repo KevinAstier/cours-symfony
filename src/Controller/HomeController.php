27aa2d737669a05fdd798076d3a5c9b49c36e79f
KevinAstier <kevin.astier@humansconnexion.com>
2022-07-28 17:13:43 +0200

    Fin Doctrine - mise en place entités et premiers affichages Twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,20 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController {
     /**
      * @Route("/", name="homepage")
-     * @return void
      */
-    public function homepage() {
-        return $this->render('home.html.twig');
+    public function homepage(ProductRepository $productRepository) {
+
+        $products = $productRepository->findBy([], [], 3);
+
+        return $this->render('home.html.twig', [
+            'products' => $products
+        ]);
     }
 }
