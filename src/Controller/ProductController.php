674ac9b92526777bc4607f8ebc546c8b8f31f982
KevinAstier <kevin.astier@humansconnexion.com>
2022-07-29 11:10:52 +0200

    Twig 2 - Page produit et gestion des url

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,10 +3,12 @@
 namespace App\Controller;
 
 use App\Repository\CategoryRepository;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class ProductController extends AbstractController
 {
@@ -24,6 +26,22 @@ class ProductController extends AbstractController
         return $this->render('product/category.html.twig', [
             'slug' => $slug,
             'category' => $category
+        ]);
+    }
+
+    #[Route('/{category_slug}/{slug}', name: 'product_show')]
+    public function show($slug, ProductRepository $productRepository){
+
+        $product = $productRepository->findOneBy([
+            'slug' => $slug
+        ]);
+
+        if(!$product) {
+            throw $this->createNotFoundException("Le produit demandé n'existe pas");
+        }
+
+        return $this->render('product/show.html.twig', [
+            'product' => $product
         ]);
     }
 }
