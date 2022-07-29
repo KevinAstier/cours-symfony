4ae0090d9ccbdf37280f2ee312d43dd11b31b2ba
KevinAstier <kevin.astier@humansconnexion.com>
2022-07-29 17:23:37 +0200

    Template de fomulaire et début traitement requête

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,9 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Entity\Product;
 use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
@@ -42,6 +51,53 @@ class ProductController extends AbstractController
 
         return $this->render('product/show.html.twig', [
             'product' => $product
+        ]);
+    }
+
+    #[Route('/admin/product/create', name: 'product_create')]
+    public function create(FormFactoryInterface $factory, Request $request) {
+        dump($request);
+        $builder = $factory->createBuilder();
+        $builder->add('name', TextType::class, [
+            'label' => 'Nom du produit',
+            'attr' => ['placeholder' => 'Tapez le nom du produit']
+         ])
+            ->add('shortDescription', TextareaType::class, [
+                'label' => 'Description courte',
+                'attr' => ['placeholder' => 'Tapez une description courte mais parlante'
+                ]
+            ])
+            ->add('price', MoneyType::class, [
+                'label' => 'Prix du produit',
+                'attr' => ['placeholder' => 'Tapez le prix du produit']
+            ])
+            ->add('category', EntityType::class, [
+                'label' => 'Catégorie',
+                'placeholder' => '-- Choisir une catégorie --',
+                'class' => Category::class,
+                'choice_label' => 'name'
+            ]);
+
+        $form = $builder->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()) {
+            $data = $form->getData();
+
+            $product = new Product();
+            $product->setName($data['name'])
+                ->setShortDescription(($data['shortDescription']))
+                ->setPrice($data['price'])
+                ->setCategory($data['category']);
+
+            dd($product);
+        }
+
+        $formView = $form->createView();
+
+        return $this->render('product/create.html.twig', [
+            'formView' => $formView
         ]);
     }
 }
